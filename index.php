8c78b35b04aec471e3ea7e5baa58fd223f9e336b
Serkan Danışan <serkan.danisan@gmail.com>
2021-04-11 21:53:02 +0300

    Select words with letters count, spesific patterns

--- index.php
+++ index.php
@@ -39,3 +39,20 @@ echo preg_replace($patternNotation4_2,"*",$str4).'<br>';
 echo preg_replace($patternNotation4_3,"*",$str4).'<br>';
 echo preg_replace($patternNotation4_4,"*",$str4).'<br>';
 echo preg_replace($patternNotation4_5,"*",$str4).'<br><br>';
+
+
+
+// .{}
+$str5 = "Alooo sds ddds x Ads 9ds ?ds gray grey";
+$patternNotation5 = "/.{2,5}/"; // Select words with 5 letters, if dont it can be 2,3,4
+$patternNotation5_1 = "/.{3,}/";// Select words with at least 3 letters, it can be more and more 
+$patternNotation5_2 = "/[sd]ds/"; //Select same endings and use it
+$patternNotation5_3 = "/[a-zA-Z0-9?_]ds/";//Select same endings and use it with range
+$patternNotation5_4 = "/gr(a|e)y/"; //Grouping the words
+$patternNotation5_5 = "/\D/";
+echo preg_replace($patternNotation5,"*",$str5).'<br>';
+echo preg_replace($patternNotation5_1,"*",$str5).'<br>';
+echo preg_replace($patternNotation5_2,"-",$str5).'<br>';
+echo preg_replace($patternNotation5_3,"*",$str5).'<br>';
+echo preg_replace($patternNotation5_4,"&",$str5).'<br>';
+echo preg_replace($patternNotation5_4,"*",$str5).'<br><br>';
